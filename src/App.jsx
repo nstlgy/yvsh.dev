774945d8aeb867e9eba46490b6a6fb300377028f
nstlgy <yvsh7.btw@gmail.com>
2024-06-15 22:14:47 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,163 @@
+import './App.css';
+import Perspective from '../src/assets/images/BlackSquare.svg';
+
+function App() {
+  return (
+    <>
+      <div className="fade-in">
+        <div className="wrapper relative flex items-center justify-center w-full">
+          <main className="relative flex w-full max-w-[640px] flex-col justify-start gap-8 px-8 py-8">
+            <nav className="flex justify-center">
+              <ul className="flex gap-5 font-mono font-thin">
+                <li>
+                  <a href="">/whoami</a>
+                </li>
+                <li>
+                  <a href="">/bookmarks</a>
+                </li>
+                <li>
+                  <a href="">/contact</a>
+                </li>
+              </ul>
+            </nav>
+            <div className="content relative flex flex-col gap-8">
+              <div className="flex w-full flex-col gap-2">
+                <img
+                  className="custom-image flex size-14 justify-start"
+                  src={Perspective}
+                  alt="Dark Black square svg"
+                  style={{
+                    boxSizing: 'border-box',
+                    cursor: 'pointer',
+                  }}
+                />
+                <div className="flex flex-col gap-4">
+                  <h1 className="home font-bold flex items-center">
+                    Yash Singh
+                    <span className="online-indicator"></span>
+                  </h1>
+                  <div className="font-light">
+                    Minimalist Creator / (ˈmɪnɪməlɪst krɪˈeɪtər) / noun
+                  </div>
+                  <div className="font-light">
+                    An individual who employs simplicity and essential elements
+                    to produce clear, impactful works of art.
+                  </div>
+                  <div className="font-light">
+                    A creator who embraces the philosophy of 'less is more,'
+                    focusing on purity and clarity in their creative process.
+                  </div>
+                  <div className="font-light">
+                    A multidisciplinary artist who transcends boundaries,
+                    creating without limitations.
+                  </div>
+                  <div className="flex flex-wrap gap-4">
+                    <a href="">Email</a>
+                    <a href="">Twitter</a>
+                    <a href="">Github</a>
+                    <a href="">Works</a>
+                    <a href="">CV</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="projects flex w-full flex-col gap-2">
+              <div className="flex flex-col gap-4">
+                <h1>Projects</h1>
+              </div>
+              <div className="blur-container">
+                <a
+                  href=""
+                  className="blur-item -mx-4 flex items-center gap-2 whitespace-nowrap text-nowrap rounded-lg px-4 py-3 transition-all hover:bg-gray-100/50 font-extralight"
+                >
+                  Birthday Wishes
+                  <div
+                    data-orientation="horizontal"
+                    role="none"
+                    className="bg-border h-[1px] w-full shrink"
+                  >
+                    <hr />
+                  </div>
+                  January 2025
+                </a>
+                <a
+                  href=""
+                  className="blur-item -mx-4 flex items-center gap-2 whitespace-nowrap text-nowrap rounded-lg px-4 py-3 transition-all hover:bg-gray-100/50 font-extralight"
+                >
+                  Another Project
+                  <div
+                    data-orientation="horizontal"
+                    role="none"
+                    className="bg-border h-[1px] w-full shrink"
+                  >
+                    <hr />
+                  </div>
+                  February 2025
+                </a>
+              </div>
+            </div>
+
+            <div className="playground flex w-full flex-col gap-2">
+              <div className="flex flex-col gap-4">
+                <h1>Playground</h1>
+              </div>
+              <div className="blur-container">
+                <a
+                  href=""
+                  className="blur-item -mx-4 flex items-center gap-2 whitespace-nowrap text-nowrap rounded-lg px-4 py-3 transition-all hover:bg-gray-100/50 font-extralight"
+                >
+                  Playground Item 1
+                  <div
+                    data-orientation="horizontal"
+                    role="none"
+                    className="bg-border h-[1px] w-full shrink"
+                  >
+                    <hr />
+                  </div>
+                  March 2025
+                </a>
+                <a
+                  href=""
+                  className="blur-item -mx-4 flex items-center gap-2 whitespace-nowrap text-nowrap rounded-lg px-4 py-3 transition-all hover:bg-gray-100/50 font-extralight"
+                >
+                  Playground Item 2
+                  <div
+                    data-orientation="horizontal"
+                    role="none"
+                    className="bg-border h-[1px] w-full shrink"
+                  >
+                    <hr />
+                  </div>
+                  April 2025
+                </a>
+                <a
+                  href=""
+                  className="blur-item -mx-4 flex items-center gap-2 whitespace-nowrap text-nowrap rounded-lg px-4 py-3 transition-all hover:bg-gray-100/50 font-extralight"
+                >
+                  Playground Item 3
+                  <div
+                    data-orientation="horizontal"
+                    role="none"
+                    className="bg-border h-[1px] w-full shrink"
+                  >
+                    <hr />
+                  </div>
+                  May 2025
+                </a>
+              </div>
+            </div>
+
+            <footer className="justify-center flex text-xs">
+              <p className="font-extralight">Designed by &nbsp;</p>
+              <p className="font-semibold">Yash Singh &nbsp;</p>
+              <p className="font-extralight">with a bit of ☕️</p>
+            </footer>
+          </main>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/SideMenu.jsx
@@ -0,0 +1,5 @@
+function SideMenu() {
+  return <></>;
+}
+
+export default SideMenu;
